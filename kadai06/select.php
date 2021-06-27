4f81e4bf738a948505e5154dc356899eb8128abe
owen0618 <83898534+owen0618@users.noreply.github.com>
2021-06-27 10:06:17 +0900

    Add files via upload

---
+++ kadai06/select.php
@@ -0,0 +1,67 @@
+<?php
+//1.  DB接続します xxxにDB名を入れます MAMPは’’にrootと入力する
+try {
+$pdo = new PDO('mysql:dbname=kadai06;charset=utf8;host=localhost','root','root');
+} catch (PDOException $e) {
+  exit('データベースに接続できませんでした。'.$e->getMessage());
+}
+
+//２．データ登録SQL作成
+//作ったテーブル名を書く場所  xxxにテーブル名を入れます(stmtアロー演算子。prepare「SQLを準備します」の意。)
+$stmt = $pdo->prepare("SELECT * FROM a_table");
+$status = $stmt->execute();
+
+//３．データ表示
+$view="";
+if($status==false){
+  //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("ErrorQuery:".$error[2]);
+}else{
+  //Selectデータの数だけ自動でループしてくれる $resultの中に「カラム名」が入ってくるのでそれを表示させる例
+  while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
+   
+    $view .= "<p>";
+    $view .= $result["id"].":".$result["position"].":".$result["name"];
+    $view .= "</p>";
+    $view .= $result["strong"];
+    $view .= "</p>";
+  }
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>選手登録データ表示</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">データ登録</a>
+      </div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] $view-->
+<!--HTMLに埋め込む（接続。?=$view?-->
+<div>
+    <div class="container jumbotron"><?=$view?></div>
+</div>
+<!-- Main[End] -->
+
+</body>
+</html>
